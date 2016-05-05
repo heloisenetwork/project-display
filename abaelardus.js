b1a075eef523356a793bd99d95f4f43f0ee6a568
landkommunenhippie <hahn.tobias66@gmail.com>
2016-05-05 19:44:58 +0200

    Created initial abaelardus app

---
+++ abaelardus.js
@@ -0,0 +1,136 @@
+var resultSkeleton = {};
+var resultSize = 5;
+$('document').ready(function(){
+	$('#startQuery').click(askNewQuestion);
+	resultSkeleton = $('.result');
+});
+
+var askNewQuestion = function(clickEvent){
+	$('#page').val('0');
+	askHeloiseRubbered(clickEvent);
+}
+
+var askHeloiseRubbered = function(clickEvent){
+	clickEvent.preventDefault();
+	query = $('#query').val();
+	firstResult = parseInt($('#page').val()) * resultSize;
+	$.ajax({
+		url: "http://localhost:9200/heloise/CPL/_search",
+		data: {
+			q : query,
+			size: resultSize,
+			from: firstResult,
+			},
+		crossDomain: "true",
+		jsonp: "false",
+		dataType: "json",
+		success: updateResultset, 
+	});
+}
+
+function updateResultset(data){
+	$('#results').hide(0);
+	appendNewResults(data);
+	$('#results').show(1000);
+}
+
+function appendNewResults(data){
+	resultWrapper = $('#results');
+	resultWrapper.html('');
+
+	results = data.hits.hits;
+	if(results && results.length > 0){
+		results.forEach(function(rawRes, index){
+			res = rawRes._source;
+			resultStub = $(resultSkeleton.clone());
+			
+			appendEntryHeadTo(resultStub, res, rawRes);		
+			appendMetadataTo(resultStub, res);
+
+
+			resultWrapper.append(resultStub);
+			
+			});
+			resultWrapper.append(newPagination(data.hits.total));
+			
+	}
+	else{
+			resultWrapper.append($('<h2/>').text('Leider habe ich nichts gefunden.'));
+	}
+}
+
+function appendEntryHeadTo(stub, res, rawRes){
+	listEntryHead = stub.children('h2').children('a');
+	listEntryHead.text(res.name);
+	listEntryHead.attr('href', res.url);
+	listEntryHead.attr('alt', res.name + " aus dem Katalog von " + rawRes._type);
+
+}
+
+function appendMetadataTo(stub, res){
+	metadata = resultStub.children('.entrymetadata');
+
+	leftMeta = $('<div class="left" />');
+	leftMeta.append(newMetadataPair("ID",res.id));
+	leftMeta.append(newMetadataPair("Titel",res.title));
+
+	rightMeta = $('<div class="right" />');
+	rightMeta.append(newMetadataPair("Geburtstag/-Ort",res.birthDate + "/" + res.birthCity));
+	rightMeta.append(newMetadataPair("Todestag/-Ort",res.deathDate + "/" + res.deathCity));
+
+	metadata.append(leftMeta);
+	metadata.append(rightMeta);
+
+}
+
+function newMetadataPair(title,content){
+	wrapper = $('<div />'); 
+	wrapper.append(newCategoryTitle(title));
+	wrapper.append(newCategoryContent(content));
+	return wrapper;
+}
+
+function newCategoryTitle(title){
+	return $('<span />').addClass('cat-title').text(title + ":");
+}
+function newCategoryContent(content){
+	return $('<span />').addClass('cat-content').text(content);
+}
+
+function newPagination(nrOfHits){
+	nrOfPages = parseInt((nrOfHits / resultSize)-1);
+	list = $('<ul />').addClass('pagination');
+	for(i = 0; i < nrOfPages; i++ ){
+		aElement = $('<a/>').attr('href',i).text(i + 1);
+		addClassIfActive(aElement);
+		list.append($('<li/>').append(aElement));	
+	}
+	if(nrOfHits % resultSize != 0 ){
+		aElement = $('<a/>').attr('href',nrOfPages).text(nrOfPages + 1);
+		addClassIfActive(aElement);
+		list.append($('<li/>').append(aElement));	
+	}
+	addListenerTo(list);
+	return list;
+
+}
+
+function addClassIfActive(aElement){
+
+	if(aElement.attr('href')  == $('#page').val()){
+		aElement.addClass('activePage');
+	}
+}
+
+function addListenerTo(pagination){
+	$.each(pagination.children('li'),function(index, listElement){
+		$(listElement).children('a').click(navigate);
+	});
+}
+
+var navigate = function(clickEvent){
+	
+	$('#page').val($(clickEvent.target).attr('href'));
+	askHeloiseRubbered(clickEvent);
+
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Welcome to Abaelardus
+**This app is an simple Webfrontend of the Heloise-Network.**
+
+It was created to explore the indexed data of Heloise Projectpartner. It is meant as an first step to make the aggregated data visible to the user.
+
+## Functions
+
+Abaelardus just displays the data of a **running** *elasticsearch* instance. Elasticsearch must run on localhost:9200 at the moment.
+
+## Installation
+
+There are no steps necessary to make the app run, since it is a small HTML-Page with some simple js-functionalities. Just open it in a browser.
+
+Remember that a running elasticsearch on localhost is needed.
+
+Have a look at [the Heloise Project-Crawler](https://github.com/heloisenetwork/project-crawler) to get in touch with the index-structure of elasticsearch.
