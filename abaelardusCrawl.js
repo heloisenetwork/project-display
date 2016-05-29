38fa47106eaf599dc9f35eb5d75979fe6d5d60f7
landkommunenhippie <hahn.tobias66@gmail.com>
2016-05-29 23:09:00 +0200

    added detail crawling

--- abaelardusCrawl.js
+++ abaelardusCrawl.js
@@ -2,12 +2,22 @@ $(document).ready(function(){
 
 
 	$('#crawlCplIndex').click(startIndexCrawling);
+	$('#crawlCplDetails').click(startDetailCrawling);
 
 });
 
 var startIndexCrawling=function(){
 	$.ajax({
 			url: "http://localhost:8666/cpl/index",
+			corssDomain:'true',
+			success: function(){
+				alert("success");
+			}
+
+	});
+var startDetailCrawling=function(){
+	$.ajax({
+			url: "http://localhost:8666/cpl/details",
 			corssDomain:'true',
 			success: function(){
 				alert("success");
